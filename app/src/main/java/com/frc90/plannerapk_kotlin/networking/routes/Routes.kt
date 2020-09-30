9a61fb3dfbe7fa8826792e71d3a46dad6412ea89
frc_90 <francisco.padron86@gmail.com>
2020-09-30 13:17:19 -0700

    Limpiando el code

--- app/src/main/java/com/frc90/plannerapk_kotlin/networking/routes/Routes.kt
+++ app/src/main/java/com/frc90/plannerapk_kotlin/networking/routes/Routes.kt
@@ -1,11 +1,6 @@
 package com.frc90.plannerapk_kotlin.networking.routes
 
 interface Routes {
-//    companion object{
-//        const val BASE_URL = "https://planificacion.reduc.edu.cu/api/"
-//        const val CREATE_TOKEN = "token/"
-//        const val CURRENT_MONTH = "activities/current_month/"
-//    }
 
     companion object{
         const val BASE_URL = "https://planificacion.reduc.edu.cu/api/"
